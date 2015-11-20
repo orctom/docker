98507697370ca7011092ceebdf0b2f5881f2adae
Hao <orctom@gmail.com>
2015-11-20 17:21:01 +0800

    ubuntu-1.638

--- jenkins/ubuntu-1.638/bootstrap.sh
+++ jenkins/ubuntu-1.638/bootstrap.sh
@@ -5,11 +5,16 @@ set -e
 # generate ssh key if not exist
 /etc/.ssh/gen_ssh_key.sh
 
-if [ $1 == "-d" ]; then
+if [ "$1" = "-d" ]; then
 	java -jar /usr/share/jenkins/jenkins.war
 fi
 
-if [ $1 == "-sh" ] || [ $1 == "sh" ]; then
+if [ "$1" = "-sh" ] || [ "$1" = "sh" ]; then
   java -jar /usr/share/jenkins/jenkins.war &
   /bin/sh
+fi
+
+if [ "$1" = "-bash" ] || [ "$1" = "bash" ]; then
+  java -jar /usr/share/jenkins/jenkins.war &
+  /bin/bash
 fi
